3bb5cd5f7ea7c3cd765786e785673d542b70aefe
Vladyslav <vladyslav.work@gmail.com>
2020-08-02 00:45:24 +0300

    create empty methods then,catch and finally

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-class CustomPromise {}
+class CustomPromise {
+  constructor(executor) {
+    executor();
+  }
+  then() {}
+  catch() {}
+  finally() {}
+}
+
+const promise = new Promise((resolse, reject) => {
+  setTimeout(() => {
+    resolve("TDD");
+  }, 150);
+});
+
+promise
+  .then((test) => {
+    console.log("Done!");
+  })
+  .catch((error) => {
+    console.error("Error", error).finally(() => console.log("Finally"));
+  });
 
 module.exports = CustomPromise;
--- main.test.js
+++ main.test.js
@@ -1,8 +1,26 @@
 const CustumPromise = require("./main");
 
 describe("Custom promise: ", () => {
+  let promise;
+  let executor;
+
+  beforeEach(() => {
+    executor = jest.fn(() => {});
+    promise = new CustumPromise(executor);
+  });
+
   test("Should be exist and to be typeof fuction", () => {
     expect(CustumPromise).toBeDefined();
     expect(typeof CustumPromise).toBe("function");
+  });
+
+  test("Intance shold have methods: then,cath, finally", () => {
+    expect(promise.then).toBeDefined();
+    expect(promise.catch).toBeDefined();
+    expect(promise.finally).toBeDefined();
+  });
+
+  test("Shold call executor function", () => {
+    expect(executor).toHaveBeenCalled();
   });
 });
