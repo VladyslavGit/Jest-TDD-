10775ae65e4a16ca67fe6cb3ebdeb44f09dc07c2
Vladyslav <vladyslav.work@gmail.com>
2020-08-02 22:57:42 +0300

    custum class promise

--- main.js
+++ main.js
@@ -1,24 +1,55 @@
+function noop() {}
+
 class CustomPromise {
   constructor(executor) {
-    executor();
+    this.queue = [];
+    this.handleError = noop;
+    this.finallyHandler = noop;
+    try {
+      executor.call(null, this.onResolve.bind(this), this.onReject.bind(this));
+    } catch (err) {
+      this.handleError(err);
+    } finally {
+      this.finallyHandler();
+    }
   }
-  then() {}
-  catch() {}
-  finally() {}
+
+  onResolve(data) {
+    this.queue.forEach((callback) => {
+      data = callback(data);
+    });
+    this.finallyHandler();
+  }
+  onReject(error) {
+    this.handleError(error);
+    this.finallyHandler();
+  }
+
+  then(fn) {
+    this.queue.push(fn);
+    return this;
+  }
+  catch(fn) {
+    this.handleError = fn;
+    return this;
+  }
+  finally(fn) {
+    this.finallyHandler = fn;
+    return this;
+  }
 }
 
-const promise = new Promise((resolse, reject) => {
+const promise = new CustomPromise((resolve, reject) => {
   setTimeout(() => {
     resolve("TDD");
   }, 150);
 });
 
 promise
-  .then((test) => {
-    console.log("Done!");
-  })
-  .catch((error) => {
-    console.error("Error", error).finally(() => console.log("Finally"));
-  });
+  .then((test) => test.toUpperCase())
+  .then((test) => console.log("Done!", test))
+  .catch((error) =>
+    console.error("Error", error).finally(() => console.log("Finally"))
+  );
 
 module.exports = CustomPromise;
--- main.test.js
+++ main.test.js
@@ -3,9 +3,13 @@ const CustumPromise = require("./main");
 describe("Custom promise: ", () => {
   let promise;
   let executor;
+  const succesResult = 42;
+  const errorResult = "I am error";
 
   beforeEach(() => {
-    executor = jest.fn(() => {});
+    executor = jest.fn((resolve) =>
+      setTimeout(() => resolve(succesResult), 150)
+    );
     promise = new CustumPromise(executor);
   });
 
@@ -22,5 +26,29 @@ describe("Custom promise: ", () => {
 
   test("Shold call executor function", () => {
     expect(executor).toHaveBeenCalled();
+  });
+
+  test("Shold geting data in then block and do chaining", async () => {
+    const result = await promise.then((num) => num).then((num) => num * 2);
+    expect(result).toBe(succesResult * 2);
+  });
+
+  test("Shold catch error", () => {
+    const errorExecutor = (_, resolve) =>
+      setTimeout(() => resolve(errorResult), 150);
+    const errorPromise = new CustumPromise(errorExecutor);
+
+    return new Promise((resolve) => {
+      errorPromise.catch((error) => {
+        expect(error).toBe(errorResult);
+        resolve();
+      });
+    });
+  });
+
+  test("Shold call method finally", async () => {
+    const finallySpy = jest.fn(() => {});
+    await promise.finally(finallySpy);
+    expect(finallySpy).toBeCalled();
   });
 });
